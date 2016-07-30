8506d6739220692888d44caec728ef7d6a48fa36
Ajay Mayar <ajay.mayar@tufts.edu>
2016-07-30 14:56:48 -0400

    Updated lab.js

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,24 @@
-// Your JavaScript goes here...
+var request = new XMLHttpRequest();
+
+function parse(){
+	request.open("GET", "data.json", true);
+
+	request.onreadystatechange = function(){
+		if (request.readyState == 4 && request.status == 200){
+			data = request.responseText;
+			msg = JSON.parse(data);
+			elem = document.getElementById("messages");
+			for (i = 0; i < msg.length; i++){
+				elem.innerHTML += msg[i].content + " " + msg[i].username + "<br>";
+			}
+		}
+	
+		else if (request.readyState == 4 && request.status != 200){
+			document.getElementById("messages").innerHTML = "<p>Something failed</p>";
+		}
+	}
+
+	request.send(null);	
+
+
+}
