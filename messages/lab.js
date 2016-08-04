6fc9a6401d6b12862d90db21ae66273d07f54e7f
Ajay Mayar <ajay.mayar@tufts.edu>
2016-08-03 21:33:51 -0400

    Updated javaScript

--- messages/lab.js
+++ messages/lab.js
@@ -2,6 +2,8 @@ var request = new XMLHttpRequest();
 
 function parse(){
 	request.open("GET", "data.json", true);
+	// request.open("GET", "https://messagehub.herokuapp.com/messages.json", true);
+
 
 	request.onreadystatechange = function(){
 		if (request.readyState == 4 && request.status == 200){
